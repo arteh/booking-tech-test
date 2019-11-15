1aca65b569b36e014b60f7e4e09859fcca8fd4ba
Hibs <hibo.arteh@intouchnetworks.com>
2019-11-15 17:24:22 +0000

    Completed part 1 of test; desktop styles and accessibility.

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export const SearchInput = () => {
-    return (
-        <div>
-            <form className="search-form">
-                <label>Pick-up Location</label><br/>
-                
-                <input
-                placeholder="city, airport, station, region, district..."
-                >
-                </input>
+  return (
+    <div>
+      <form className="search-form">
+        <label htmlFor="Pick-up Location">Pick-up Location</label>
+        <br />
 
-            </form>
-        </div>
-    )
-}
+        <input
+          name="search-input"
+          id="search-input"
+          aria-label="Pick-up Location"
+          type="text"
+          placeholder="city, airport, station, region, district..."
+        ></input>
+      </form>
+    </div>
+  );
+};
